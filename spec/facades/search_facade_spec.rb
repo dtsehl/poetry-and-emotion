7903b079ec9549fba4289ce1f4470e16a19a2e9f
Dan Sehl <dan.sehl@gmail.com>
2020-09-11 09:52:03 -0600

    Refactor Search Facade test

--- spec/facades/search_facade_spec.rb
+++ spec/facades/search_facade_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe SearchFacade do
   describe "class methods" do
     it "ten_poems_by_author" do
-      results = SearchFacade.ten_poems_by_author(@author)
+      author = "Emily"
+      results = SearchFacade.ten_poems_by_author(author)
       expect(results.size).to eq(10)
       expect(results).to be_a(Array)
       expect(results[0]).to be_a(Poem)
