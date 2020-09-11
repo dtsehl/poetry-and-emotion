e04d73c3e26045da260a211d550aad8d8e9bea0c
Dan Sehl <dan.sehl@gmail.com>
2020-09-11 10:56:07 -0600

    Add method add_tones to Poem PORO

--- app/poros/poem.rb
+++ app/poros/poem.rb
@@ -1,9 +1,16 @@
 class Poem
-  attr_reader :title, :author, :lines
+  attr_reader :title, :author, :lines, :tones
 
   def initialize(info)
     @title = info[:title]
     @author = info[:author]
-    @lines = info[:lines]
+    @lines = info[:lines].join(" ")
+    @tones = []
+  end
+
+  def add_tones(tones)
+    tones[:document_tone][:tones].each do |tone|
+      @tones << tone[:tone_name]
+    end
   end
 end
