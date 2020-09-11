d37e3c911643f2fa9c020d2104672391bcfb073e
Dan Sehl <dan.sehl@gmail.com>
2020-09-11 10:56:22 -0600

    Add get tone of poems method to Search Facade

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,10 +1,20 @@
 class SearchFacade
   def self.ten_poems_by_author(author)
     results = PoetryService.poems_by_author(author)
-    10.times do
+    poems = []
+    # I do not know why this if statement is not being followed so I had to add the take at the bottom.
+    if poems.size <= 10
       results.each do |poem_data|
-        Poem.new(poem_data)
+        poems << Poem.new(poem_data)
       end
+    end
+    get_tone_of_poems(poems.take(10))
+  end
+
+  def self.get_tone_of_poems(poems)
+    poems.each do |poem|
+      tones = ToneService.get_tone_of_poem(poem)
+      poem.add_tones(tones)
     end
   end
 end
