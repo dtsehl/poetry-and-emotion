a2678c7125eaa56ac473c0807f54559f8a421561
Dan Sehl <dan.sehl@gmail.com>
2020-09-11 09:52:23 -0600

    Refactor ten_poems_by_author class method in Search Facade

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,6 +1,10 @@
 class SearchFacade
   def self.ten_poems_by_author(author)
     results = PoetryService.poems_by_author(author)
-    
+    10.times do
+      results.each do |poem_data|
+        Poem.new(poem_data)
+      end
+    end
   end
 end
